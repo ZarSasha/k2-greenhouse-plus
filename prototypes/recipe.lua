02fc374b92e0ec76ababce5c23fbe46994b333af
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-08-10 22:31:01 +0200

    Update recipe.lua

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -17,49 +17,50 @@ end
 local function createGreenhouseRecipe(Variant, Order)
     -- Chooses only one glass item name and amount to be used, from among various mods. Ordered so
     -- that smaller mods and mods that modify other mods go first.
-    local Glass = {"iron-plate",  32}
-    local raw_item = {
-        ["glass"] = data.raw.item["glass"],
-        ["glass-plate"] = data.raw.item["glass-plate"]
-    }
-    if     mods["Glass"] then
-        if raw_item["glass-plate"] then
-            Glass = {"glass-plate", 32} -- 100% glass : stone
+
+    local function item_exists(ModName, Item)
+        if not data.raw.item[Item] then
+            log("item with ID \""..Item.."\" from the mod \""..ModName.."\" does not exist!")
+            return false
         else
-            log("item with ID \"glass-plate\" from the mod \"Glass\" does not exist!")
-        end
-    elseif mods["quirkycat_glass"] then
-        if raw_item["glass"] then
-            Glass = {"glass",       48} -- 150% glass : stone
-        else
-            log("item with ID \"glass\" from the mod \"quirkycat_glass\" does not exist!")
-        end
-    elseif mods["crushing-industry"] and settings.startup["crushing-industry-glass"].value then
-        if raw_item["glass"] then
-            Glass = {"glass",       25} --  80% glass : stone
-        else
-            log("item with ID \"glass\" from the mod \"crushing-industry\" does not exist!")
-        end
-    elseif mods["factorioplus"] then
-        if raw_item["glass-plate"] then
-            Glass = {"glass-plate", 32} -- 100% glass : stone
-        else
-            log("item with ID \"glass-plate\" from the mod \"factorioplus\" does not exist!")
-        end
-    elseif mods["aai-industry"] then
-        if raw_item["glass"] then
-            Glass = {"glass",       16} --  50% glass : stone
-        else
-            log("item with ID \"glass\" from the mod \"aai-industry\" does not exist!")
+            return true
         end
     end
+
+    local Glass = {"iron-plate",  32}
+
+    if     mods["Glass"] then
+        if item_exists("Glass", "glass-plate") then
+            Glass = {"glass-plate", 32} -- 100% glass : stone
+        end
+    elseif mods["quirkycat_glass"] then
+        if item_exists("quirkycat_glass", "glass") then
+            Glass = {"glass",       48} -- 150% glass : stone
+        end
+    elseif mods["crushing-industry"] and settings.startup["crushing-industry-glass"].value then
+        if item_exists("crushing-industry", "glass") then
+            Glass = {"glass",       25} --  80% glass : stone
+        end
+    elseif mods["factorioplus"] then
+        if item_exists(factorioplus, "glass-plate") then
+            Glass = {"glass-plate", 32} -- 100% glass : stone
+        end
+    elseif mods["aai-industry"] then
+        if item_exists("aai-industry", "glass") then
+            Glass = {"glass",       16} --  50% glass : stone
+        end
+    end
+
     local TreeSeed = SPACE_AGE and "tree-seed" or "wood" -- assumes 1 wood to 1 seed
+
     local Set  = SETTING.GLEBA_GREENHOUSES_1
+
     local Soil = {
         ["disabled"]             = "overgrowth", -- value doesn't matter, greenhouse is disabled
         ["with-overgrowth-soil"] = "overgrowth",
         ["with-artificial-soil"] = "artificial"
     }
+
     local Crop = {
         ["tree"]        = { seed = {TreeSeed,       10}, soil = {"landfill",                  1} },
         ["yumako-tree"] = { seed = {"yumako-seed",   5}, soil = {Soil[Set].."-yumako-soil",   8} },
@@ -67,6 +68,7 @@ local function createGreenhouseRecipe(Variant, Order)
         ["slipstack"]   = { seed = {"spoilage",     50}, soil = {"landfill",                  1} },
         ["sunnycomb"]   = { seed = {"spoilage",     50}, soil = {"landfill",                  1} }
     }
+
     local output = {
         type     = "recipe",
         name     = PREFIX.."greenhouse-for-"..Variant,
@@ -103,7 +105,9 @@ local function createCropGrowthRecipe(Variant, Order)
                              scale = 0.300, shift = {-8, 0}, draw_background = true }},
         ["sunnycomb"]   = {{ icon = "__space-age__/graphics/icons/spoilage.png"     }},
     }
+
     local Subgroup      = SPACE_AGE and "agriculture-processes" or "raw-resource"
+
     local Results = {
         ["tree"]        = {{ type = "item",  name = "wood",     amount =  5 }},
         ["yumako-tree"] = {{ type = "item",  name = "yumako",   amount =  5 }},
@@ -112,6 +116,7 @@ local function createCropGrowthRecipe(Variant, Order)
                            { type = "item",  name = "stone",    amount =  2 }},
         ["sunnycomb"]   = {{ type = "item",  name = "spoilage", amount =  5 }},
     }
+
     local output = {
         type     = "recipe",
         name     = PREFIX.."greenhouse-"..Variant.."-growth",
