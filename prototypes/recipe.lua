6e6e995d520fd7cc2b44434a93213e20c289ba10
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-17 00:56:13 +0200

    Update recipe.lua

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -199,7 +199,7 @@ local AdvancedWoodPyrolysisRecipe = {
         { icon = "__base__/graphics/icons/wood.png",
           scale = 0.275, shift = {-3, -3}, draw_background = true }
     },
-    categories = {"chemistry, organic"},
+    categories = {"chemistry", "organic"},
     subgroup = "fluid-recipes",
     order    = "a[fluid]-b[oil]-b[petroleum-gas]",
     enabled = false,
