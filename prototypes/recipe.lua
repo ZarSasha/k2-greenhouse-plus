51d26a89b17358b7c5afc7434a7c36b8b70ea248
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-17 16:31:53 +0200

    test

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -174,12 +174,12 @@ local BasicWoodPyrolysisRecipe = {
     subgroup = "raw-material",
     order    = "a[burning]-a[charcoal]",
     enabled = true, -- Unlocked right from the start.
-    energy_required = 6.4, -- 4.8 at double speed
+    energy_required = 4.8, -- 2.4 at double speed
     ingredients = {
-        { type = "item", name = "wood", amount = 8 }
+        { type = "item", name = "wood", amount = 6 }
     },
     results = {
-        { type = "item", name = "coal", amount = 4 }
+        { type = "item", name = "coal", amount = 3 }
     },
     allow_productivity = true
 }
@@ -204,9 +204,9 @@ local AdvancedWoodPyrolysisRecipe = {
         { type = "item",  name = "wood",          amount = 15 }
     },
     results = {
-        { type = "fluid", name = "crude-oil",     amount = 20,  fluidbox_index = 2 },
+        { type = "fluid", name = "crude-oil",     amount = 30,  fluidbox_index = 2 },
         { type = "fluid", name = "petroleum-gas", amount = 10,  fluidbox_index = 1 },
-        { type = "item",  name = "coal",          amount =  4 }
+        { type = "item",  name = "coal",          amount =  3 }
     },
     crafting_machine_tint = {
         primary    = {r = 0.250, g = 0.200, b = 0.250, a = 1.000}, -- Liquid.     1st output color?
