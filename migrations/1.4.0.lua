694d235c9fb487775da55c0a9dc66c61ad69172c
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-17 16:45:20 +0200

    fixes

---
+++ migrations/1.4.0.lua
@@ -0,0 +1,9 @@
+---------------------------------------------------------------------------------------------------
+-- MIGRATIONS FOR V1.4.0
+---------------------------------------------------------------------------------------------------
+
+game.print("[color=acid]Krastorio 2 Greenhouse + Wood Pyrolysis:[/color]")
+game.print("  v1.4.0: NEW FEATURE: The mod now provides its own glass for the greenhouses")
+game.print("  if no other mods do so. This can be disabled in the startup settings.")
+
+---------------------------------------------------------------------------------------------------
