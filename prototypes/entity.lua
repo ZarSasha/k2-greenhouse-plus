0d668908a4ef486f773e963c510b1ceccc3a2b6c
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-22 15:51:57 +0200

    Update entity.lua

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -46,7 +46,7 @@ local function createGreenhouse(Variant)
     }
     -- AUXILIARY TABLES --
     local PassiveEmissions = {
-        ["tree"]        = {pollution = (-2.25 / 60)}
+        ["tree"]        = {pollution = -(2.25/60)}
     }
     local ActiveEmissions = {
         ["yumako-tree"] = {spores = 18.5 * SETTING.OUTPUT_RATE["yumako-tree"]},
