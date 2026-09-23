7582c571c7929dba55fe180e4b03ba2a9b5f913e
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-23 23:58:41 +0200

    Update recipe.lua

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -173,14 +173,15 @@ local BasicWoodPyrolysisRecipe = {
     subgroup = "raw-material",
     order    = "a[burning]-a[charcoal]",
     enabled = true, -- Unlocked right from the start.
-    energy_required = 7*2, -- 7.0 at double speed (electric furnace)
+    energy_required = 4.8, -- 2.4 at double speed (electric furnace)
     ingredients = {
-        { type = "item", name = "wood", amount = 14 }
+        { type = "item", name = "wood", amount = 6 }
     },
     results = {
-        { type = "item", name = "coal", amount = 7 }
+        { type = "item", name = "coal", amount = 3 }
     },
-    allow_productivity = true
+    allow_productivity = true--,
+    --maximum_productivity = 0.5 -- max in vanilla: 0.5 (lv5 prod 3: 2x25% )
 }
 ---------------------------------------------------------------------------------------------------
 -- CHEMICAL PLANT: ADVANCED WOOD PYROLYSIS RECIPE
@@ -198,14 +199,14 @@ local AdvancedWoodPyrolysisRecipe = {
     subgroup = "fluid-recipes",
     order    = "a[fluid]-b[oil]-b[petroleum-gas]",
     enabled = false,
-    energy_required = 7.0, -- 3.5 at double speed (biochamber)
+    energy_required = 4.0, -- 2.0 at double speed (biochamber)
     ingredients = {
-        { type = "item",  name = "wood",          amount = 14 }
+        { type = "item",  name = "wood",          amount = 15 }
     },
     results = {
-        { type = "fluid", name = "crude-oil",     amount     = 3,                 fluidbox_index = 2 },
-        { type = "fluid", name = "petroleum-gas", amount_min = 1, amount_max = 2, fluidbox_index = 1 },
-        { type = "item",  name = "coal",          amount     = 6 }
+        { type = "fluid", name = "crude-oil",     amount = 20, fluidbox_index = 2 },
+        { type = "fluid", name = "petroleum-gas", amount = 18, fluidbox_index = 1 },
+        { type = "item",  name = "coal",          amount = 3 }
     },
     crafting_machine_tint = {
         primary    = {r = 0.250, g = 0.200, b = 0.250, a = 1.000}, -- Liquid.     1st output color?
@@ -213,7 +214,8 @@ local AdvancedWoodPyrolysisRecipe = {
         tertiary   = {r = 0.875, g = 0.716, b = 0.586, a = 1.000}, -- Outer smoke. 1st input color?
         quaternary = {r = 1.000, g = 0.614, b = 0.280, a = 1.000}  -- Inner smoke. 2nd input color?
     },
-    allow_productivity = true
+    allow_productivity = true--,
+    --maximum_productivity = 1.5 -- max in vanilla: 1.5 (biochamber: 50%, lv5 prod 3: 4x25% )
 }
 
 ---------------------------------------------------------------------------------------------------
