b27b83986ef876d7ac2e208ac869a6eb94b90b85
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-24 01:58:16 +0200

    Update recipe.lua

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -206,7 +206,7 @@ local AdvancedWoodPyrolysisRecipe = {
     results = {
         { type = "fluid", name = "crude-oil",     amount = 20, fluidbox_index = 2 },
         { type = "fluid", name = "petroleum-gas", amount = 10, fluidbox_index = 1 },
-        { type = "item",  name = "coal",          amount = 2}
+        { type = "item",  name = "coal",          amount = 4 }
     },
     crafting_machine_tint = {
         primary    = {r = 0.250, g = 0.200, b = 0.250, a = 1.000}, -- Liquid.     1st output color?
