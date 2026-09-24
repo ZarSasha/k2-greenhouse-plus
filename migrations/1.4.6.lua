d69dd8013694484fe627705a9689204f3116d6bb
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-24 14:42:18 +0200

    fix

--- migrations/1.4.6.lua
+++ migrations/1.4.6.lua
@@ -3,7 +3,6 @@
 ---------------------------------------------------------------------------------------------------
 
 game.print("[color=acid]Krastorio 2 Greenhouse + Wood Pyrolysis:[/color]")
-game.print("  v1.4.6: The pyrolysis recipes were rebalanced, which also")
-game.print("  affects building ratios! See changelog for more details.")
+game.print("  v1.4.6: Pyrolysis has been rebalanced! See the changelog for more details.")
 
 ---------------------------------------------------------------------------------------------------
