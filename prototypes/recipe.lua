180d5a3b4429015943d4eb621d5029fe91a9d81c
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-24 20:01:40 +0200

    Update recipe.lua

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -239,8 +239,8 @@ local BioWoodDistillationRecipe = {
     },
     results = {
         { type = "fluid", name = "crude-oil",     amount = 20, fluidbox_index = 2 },
-        { type = "fluid", name = "petroleum-gas", amount = 10, fluidbox_index = 1 },
-        { type = "item",  name = "coal",          amount = 3 }
+        { type = "fluid", name = "petroleum-gas", amount =  5, fluidbox_index = 1 },
+        { type = "item",  name = "coal",          amount =  3 }
     },
     crafting_machine_tint = {
         primary    = {r = 0.250, g = 0.200, b = 0.250, a = 1.000}, -- Liquid.     1st output color?
