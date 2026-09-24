6b925a6e347b66d2e51a82c4707366fc1a7883db
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-24 12:16:57 +0200

    fix

--- migrations/1.4.6.lua
+++ migrations/1.4.6.lua
@@ -3,7 +3,7 @@
 ---------------------------------------------------------------------------------------------------
 
 game.print("[color=acid]Krastorio 2 Greenhouse + Wood Pyrolysis:[/color]")
-game.print("  v1.4.6: The pyrolysis recipes were rebalanced, which affects important building ratios!")
-game.print("  See changelog for more details.")
+game.print("  v1.4.6: The pyrolysis recipes were rebalanced, which also")
+game.print("  affects building ratios! See changelog for more details.")
 
 ---------------------------------------------------------------------------------------------------
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -215,7 +215,7 @@ local AdvancedWoodPyrolysisRecipe = {
         quaternary = {r = 1.000, g = 0.614, b = 0.280, a = 1.000}  -- Inner smoke. 2nd input color?
     },
     allow_productivity = true,
-    maximum_productivity = 1.25 -- max in vanilla: 1.5 (biochamber: 50%, lv5 prod 3: 4x25% )
+    maximum_productivity = 1.0 -- max in vanilla: 1.5 (biochamber: 50%, lv5 prod 3: 4x25% )
 }
 
 ---------------------------------------------------------------------------------------------------
